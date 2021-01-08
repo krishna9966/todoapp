7916113fde648f89fc7eb28daa8a6b5df038e021
krishna9966 <krishna.9966426434@gmail.com>
2021-01-08 23:03:25 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import { Button, Input, FormControl, InputLabel } from "@material-ui/core";
+import {
+  Button,
+  Input,
+  FormControl,
+  InputLabel,
+  List,
+} from "@material-ui/core";
 import Todo from "./Todo";
 import db from "./firebase";
 import firebase from "firebase";
+
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
 
   //useeffect loads when the page loads first time or when the event mentioned after the function triggers
   useEffect(() => {
-    db.collection("todos").orderBy('timestamp','desc').onSnapshot((snapshot) => {
-      setTodos(snapshot.docs.map((doc) => doc.data().task));
-    });
+    db.collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setTodos(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            todo: doc.data().task,
+            timestamp: "Data",
+            // doc.data().timestamp,
+          }))
+        );
+      });
   }, []);
-
+  console.log(todos);
   // const addInput = (e) => {
   //   e.preventDefault();
   //   console.log(e.target.value);
@@ -26,16 +42,22 @@ function App() {
     //this will stop refreshing the page
     event.preventDefault();
     db.collection("todos").add({
-      task: input,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      task: input,
     });
     // setTodos([...todos, input]); //appends theinput value to end of todos
     setInput(""); // clears the input in form
   };
+  const ulstyle = {
+    display: "inline-block",
+  };
 
   return (
     <div className="App">
-      <h1>Todo App</h1>
+      <h1  style={{color:'red'}} >Todo App</h1>
+      <br/>
+      <br/>
+      <br/>
       <form action="">
         <FormControl>
           <InputLabel htmlFor="my-input"> write a todo</InputLabel>
@@ -62,9 +84,10 @@ function App() {
         </Button>
       </form>
       <br />
-      <ul>
+
+      <ul  style={ulstyle} >
         {todos.map((todo) => (
-          <Todo todo={todo} />
+          <Todo key={todo.id} todo={todo} />
         ))}
       </ul>
     </div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,104 @@
-import { List, ListItem, ListItemText } from "@material-ui/core";
-import React from "react";
+import { Button, List, ListItem, ListItemText, Modal } from "@material-ui/core";
+import React, { useState } from "react";
+import db from "./firebase";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Todo(props) {
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState("");
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const updateTodo = () => {
+    db.collection("todos").doc(props.todo.id).set(
+      {
+        task: input,
+      },
+      { merge: true }
+    );
+    setOpen(false);
+  };
+  const mystyle = {
+    display: "flex",
+    justifyContent: "space-around",
+    // width: "700px",
+    // height: "50px",
+    // alignItems:'center',
+    border: "1px solid red",
+    backgroundColor: "yellow",
+    padding: "10px",
+    borderRadius: "20px",
+    // paddingBottom: "20px",
+    // paddingLeft:'200px',
+    // paddingRight:'100px'
+  };
+
+  const mystyle1 ={
+    display: "flex",
+    justifyContent:'center',
+  }
+  // var date = new Date(props.todo.timestamp).toLocalString();
+
   return (
     <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div className={classes.paper}>
+          <h2 id="simple-modal-title">Update Todo here</h2>
+          <input
+            placeholder={props.todo.todo}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <Button onClick={updateTodo}>update</Button>
+        </div>
+      </Modal>
       <List>
-        <ListItem>
-          <ListItemText primary={props.todo} secondary="Dummy DeadLine" />
-        </ListItem>
+        <div style={mystyle}>
+          <ListItem  style={mystyle1} >
+            <ListItemText
+              primary={props.todo.todo}
+              secondary={props.todo.timestamp}
+            />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <EditIcon variant="contained" color="primary" onClick={handleOpen}>
+              Edit Me
+            </EditIcon>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <DeleteForeverIcon
+              variant="contained"
+              color="secondary"
+              onClick={(e) =>
+                db.collection("todos").doc(props.todo.id).delete()
+              }
+            >
+              Delete Me
+            </DeleteForeverIcon>
+          </ListItem>
+        </div>
       </List>
     </div>
   );
